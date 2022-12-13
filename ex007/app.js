ee548d5311cc390fc2f33c0bd0bbec493e9c41e9
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2022-12-13 20:02:37 -0300

    ex007
    exercicios 7 concluido

--- ex007/app.js
+++ ex007/app.js
@@ -1,5 +1,8 @@
 function altura(){   
-    let peso = document.getElementById("resposta").value
-    let altura = (72.7*peso) - 58
-    document.getElementById("peso").innerText = altura + " seu peso ideal"
+    let h = document.getElementById("resposta").value
+    let hh = document.getElementById("resposta").value
+    let homem = (72.7*hh) - 58
+    let mulher = (62.1*h) - 44.7
+    document.getElementById("hh").innerText = homem + " se você for homem esse é seu peso ideal"
+    document.getElementById("h").innerText = mulher+ " se você for mulher esse é seu peso ideal"
 }
